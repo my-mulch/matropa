47da58e038794f83a056ecf4849be9934b5fd482
Truman Purnell <truman.purnell@gmail.com>
2018-10-02 21:45:32 -0700

    removes interpret method from ebml specs

--- ebml/element.mjs
+++ ebml/element.mjs
@@ -4,16 +4,20 @@ export default class EBMLement {
     constructor(doc) {
         this.doc = doc
 
-        this.head = this.vint()
-        this.size = this.vint()
-        // this.data = 
+        this.head = this.vint().reduce(this.doc.specs.vintfull)
+        this.size = this.vint().reduce(this.doc.specs.vintrmlz)
+        this.data = this.mark(this.size)
+    }
+
+    mark(marker) {
+        return [this.doc.head, this.doc.head += marker]
     }
 
     vint() {
         const leadByte = this.doc.peek()
         const leadSize = this.doc.utils.leadZ(leadByte)
 
-        return [this.doc.head, this.doc.head += leadSize]
+        return this.mark(leadSize)
     }
 
     toString() { return { tags: this.tags, size: this.size, data: this.data } }
