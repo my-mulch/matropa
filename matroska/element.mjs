09fef434d07a03893d94a8fff2c02b4a69872ba6
Truman Purnell <truman.purnell@gmail.com>
2018-10-03 15:38:45 -0700

    fixes bug relating to muliplicity of certain ids

--- matroska/element.mjs
+++ matroska/element.mjs
@@ -4,10 +4,15 @@ import util from 'util'
 export default class MatroskaElement extends EBMLement {
     constructor(doc) {
         super(doc)
- 
+
         this.id = this.doc.specs.id(this.head)
         this.data = this.doc.specs[this.id.type](this)
-        this.doc[this.id.name.trim()] = this
+
+        // Expose keys
+        if (!this.doc[this.id.toString()])
+            this.doc[this.id.toString()] = []
+
+        this.doc[this.id.toString()].push(this)
     }
 
     toString() { return { id: this.id, size: this.size, data: this.data } }
